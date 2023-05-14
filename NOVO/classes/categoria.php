304b9016000dcc28eed438e798cdc9eb4f8330e7
Bruno Lima <brunolima280103@gmail.com>
2023-05-14 17:10:46 -0300

    class categoria

---
+++ NOVO/classes/categoria.php
@@ -0,0 +1,16 @@
+<?php
+
+    require_once("programa_de_milhagem.php");
+
+    class Categoria {
+        
+        private string $nome;
+        private int $pontuacao;
+
+        public function __construct( string $p_nome, int $p_pontuacao ) {
+            $this->nome = $p_nome;
+            $this->pontuacao = $p_pontuacao;
+        }
+    }
+
+?>

1c89a99491d6971e232763467f3da86ece1706fc
KalilDev <kalilgamer0@gmail.com>
2023-05-21 21:18:39 -0300

    ProgramaDeMilhagem: format and add types to getters

--- NOVO/classes/ProgramaDeMilhagem.php
+++ NOVO/classes/ProgramaDeMilhagem.php
@@ -1,39 +1,46 @@
 <?php
 
-use MyApp\Passageiro;
 
-    require_once("companha_aerea.php");
-    require_once("passageiro.php");
+require_once("passageiroVip.php");
+require_once("categoria.php");
 
-    class ProgramaDeMilhagem{
-        private array $categorias;
-        private string $nome_do_programa;
+class ProgramaDeMilhagem
+{
+    private array $categorias;
+    private string $nome_do_programa;
 
-        public function __construct(array $categoria, string $nome_do_programa)
-        {
-            $this->categorias = $categoria;
-            $this->nome_do_programa = $nome_do_programa;
-        }
-        public function getCategorias(){
-            return $this->categorias;
-        }
-        public function getNomeDoPrograma(){
-            return $this->nome_do_programa;
-        }
-        public function update(PassageiroVip $passageiro){
-            $categoriaDoPassageiro = $passageiro->getCategoriaDoPrograma();
-            $categoria = $this->categorias[0];
-            $pontuacao = $passageiro->getPontosValidos();
-            foreach($this->categorias as $categoriaAlvo){
-                if($categoriaAlvo->getPontuacao() <= $pontuacao){
-                    $categoria = $categoriaAlvo;
-                }
-            }
-            if(count($categoriaDoPassageiro) > 1 and $categoriaDoPassageiro[count($categoriaDoPassageiro)-1] == $categoria){
-                return;
-            }
-            $passageiro->alterarCategoria($categoria);
-        }
+    public function __construct(array $categoria, string $nome_do_programa)
+    {
+        $this->categorias = $categoria;
+        $this->nome_do_programa = $nome_do_programa;
     }
+
+    public function getCategorias(): array
+    {
+        return $this->categorias;
+    }
+
+    public function getNomeDoPrograma(): string
+    {
+        return $this->nome_do_programa;
+    }
+
+    public function update(PassageiroVip $passageiro): Categoria|null
+    {
+        $categoriaDoPassageiro = $passageiro->getCategoriaDoPrograma();
+        $categoria = $this->categorias[0];
+        $pontuacao = $passageiro->getPontosValidos();
+        foreach ($this->categorias as $categoriaAlvo) {
+            if ($categoriaAlvo->getPontuacao() <= $pontuacao) {
+                $categoria = $categoriaAlvo;
+            }
+        }
+        if (count($categoriaDoPassageiro) > 1 and $categoriaDoPassageiro[count($categoriaDoPassageiro) - 1] == $categoria) {
+            return null;
+        }
+        $passageiro->alterarCategoria($categoria);
+        return $categoria;
+    }
+}
 
 ?>

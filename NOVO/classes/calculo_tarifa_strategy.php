d10ab3b45de23eb3fdc89b809a47ae337ff60c1c
KalilDev <kalilgamer0@gmail.com>
2023-05-21 22:27:20 -0300

    calculo_tarifa_strategy: make it an interface aand use the aappropriate methods on FranquiasDeBagagem

--- NOVO/classes/calculo_tarifa_strategy.php
+++ NOVO/classes/calculo_tarifa_strategy.php
@@ -1,36 +1,42 @@
 <?php
-require_once 'passageiro.php';
 require_once 'franquia_de_bagagem.php';
 
-abstract class CalculoTarifaStrategy {
-    abstract function calcula(FranquiasDeBagagem $franquias);
+interface CalculoTarifaStrategy
+{
+    function calcula(FranquiasDeBagagem $franquias): float;
 }
 
-class PassageiroComumCalculoTarifaStrategy extends CalculoTarifaStrategy {
+class PassageiroComumCalculoTarifaStrategy implements CalculoTarifaStrategy
+{
     private float $tarifa;
     private float $tarifa_franquia;
 
-    public function __construct( float $tarifa, float $tarifa_franquia) {
+    public function __construct(float $tarifa, float $tarifa_franquia)
+    {
         $this->tarifa = $tarifa;
         $this->tarifa_franquia = $tarifa_franquia;
     }
 
-    public function calcula(FranquiasDeBagagem $franquias) : float{
-        return $this->tarifa * count($franquias->franquias) * $this->tarifa_franquia;
+    public function calcula(FranquiasDeBagagem $franquias): float
+    {
+        return $this->tarifa * $franquias->numeroDeFranquias() * $this->tarifa_franquia;
     }
 }
 
-class PassageiroVipCalculoTarifaStrategy extends CalculoTarifaStrategy {
+class PassageiroVipCalculoTarifaStrategy implements CalculoTarifaStrategy
+{
     private float $tarifa;
     private float $tarifa_franquia;
 
-    public function __construct(float $tarifa, float $tarifa_franquia) {
+    public function __construct(float $tarifa, float $tarifa_franquia)
+    {
         $this->tarifa = $tarifa;
         $this->tarifa_franquia = $tarifa_franquia;
     }
 
-    public function calcula(FranquiasDeBagagem $franquias) : float {
-        $franquias_a_serem_pagas = count($franquias->franquias);
+    public function calcula(FranquiasDeBagagem $franquias): float
+    {
+        $franquias_a_serem_pagas = $franquias->numeroDeFranquias();
         if ($franquias_a_serem_pagas == 0) {
             return $this->tarifa;
         }
@@ -40,10 +46,12 @@ class PassageiroVipCalculoTarifaStrategy extends CalculoTarifaStrategy {
     }
 }
 
-function calculo_tarifa_strategy_for(bool $cliente_vip, float $tarifa, float $tarifa_franquia) : CalculoTarifaStrategy {
+function calculo_tarifa_strategy_for(bool $cliente_vip, float $tarifa, float $tarifa_franquia): CalculoTarifaStrategy
+{
     if ($cliente_vip) {
         return new PassageiroVipCalculoTarifaStrategy($tarifa, $tarifa_franquia);
     }
     return new PassageiroComumCalculoTarifaStrategy($tarifa, $tarifa_franquia);
 }
+
 ?>

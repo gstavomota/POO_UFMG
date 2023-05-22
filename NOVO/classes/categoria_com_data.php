99ed2cde2551be8697df9a0856ab79eca3beef15
KalilDev <kalilgamer0@gmail.com>
2023-05-21 22:21:25 -0300

    categoria_com_data: Use DataTempo and format

--- NOVO/classes/categoria_com_data.php
+++ NOVO/classes/categoria_com_data.php
@@ -1,21 +1,26 @@
 <?php
-    require_once("categoria.php");
+require_once("categoria.php");
 
-    class CategoriaComData{
-        private Categoria $categoria;
-        private DateTime $data_de_entrada; 
+class CategoriaComData
+{
+    private Categoria $categoria;
+    private DataTempo $data_de_entrada;
 
-        public function __construct(Categoria $categoria, DateTime $data_de_entrada)
-        {
-            $this->categoria = $categoria;
-            $this->data_de_entrada = $data_de_entrada;
-        }
-        public function getCategoria(){
-            return $this->categoria;
-        }
-        public function getDataDeEntrada(){
-            return $this->data_de_entrada;
-        }
+    public function __construct(Categoria $categoria, DataTempo $data_de_entrada)
+    {
+        $this->categoria = $categoria;
+        $this->data_de_entrada = $data_de_entrada;
     }
+
+    public function getCategoria(): Categoria
+    {
+        return $this->categoria;
+    }
+
+    public function getDataDeEntrada(): DataTempo
+    {
+        return $this->data_de_entrada;
+    }
+}
 
 ?>

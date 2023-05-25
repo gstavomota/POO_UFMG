ffdc255ee46989a502dac826fc2051b7d06440db
Maria Eduarda Sampaio <202205583@cce.ufmg.br>
2023-05-25 20:30:07 -0300

    Criando classes de teste de programa de milhagem e tripulacao

---
+++ NOVO/test/programaDeMilhagem.php
@@ -0,0 +1,14 @@
+<?php
+    include_once "suite.php";
+    include_once "../classes/ProgramaDeMilhagem.php";
+
+    class ProgramaDeMilhagemTestCase extends TestCase {
+        protected function getName(): string {
+            return "programaDeMilhagem";
+        }
+
+        public function run() {
+
+        }
+    }
+?>

6cff9a75cfd1c8967407720c0a876fc4a6949049
KalilDev <kalilgamer0@gmail.com>
2023-05-25 11:45:43 -0300

    voo: some fixes

--- NOVO/classes/voo.php
+++ NOVO/classes/voo.php
@@ -11,29 +11,32 @@ require_once "temporal.php";
 class Voo
 {
     private CodigoVoo $codigo;
-    private CompanhiaAerea $companhia_aerea;
+    private SiglaCompanhiaAerea $companhia_aerea;
     private SiglaAeroporto $aeroporto_de_saida;
     private SiglaAeroporto $aeroporto_de_chegada;
-    private Data $hora_de_partida;
+    private Tempo $hora_de_partida;
     private Duracao $duracao_estimada;
-    private DiaDaSemana $dias_da_semana;
+    /**
+     * @var DiaDaSemana[]
+     */
+    private array $dias_da_semana;
     private RegistroDeAeronave $aeronave_padrao;
     private int $capacidade_passageiros;
     private float $capacidade_carga;
     private float $tarifa;
 
     public function __construct(
-        CodigoVoo          $codigo,
-        CompanhiaAerea     $companhia_aerea,
-        SiglaAeroporto     $aeroporto_de_saida,
-        SiglaAeroporto     $aeroporto_de_chegada,
-        Data               $hora_de_partida,
-        Duracao            $duracao_estimada,
-        DiaDaSemana        $dias_da_semana,
-        RegistroDeAeronave $aeronave_padrao,
-        int                $capacidade_passageiros,
-        float              $capacidade_carga,
-        float              $tarifa
+        CodigoVoo           $codigo,
+        SiglaCompanhiaAerea $companhia_aerea,
+        SiglaAeroporto      $aeroporto_de_saida,
+        SiglaAeroporto      $aeroporto_de_chegada,
+        Tempo               $hora_de_partida,
+        Duracao             $duracao_estimada,
+        array               $dias_da_semana,
+        RegistroDeAeronave  $aeronave_padrao,
+        int                 $capacidade_passageiros,
+        float               $capacidade_carga,
+        float               $tarifa
     )
     {
         $this->codigo = $codigo;
@@ -49,75 +52,125 @@ class Voo
         $this->tarifa = $tarifa;
     }
 
-    public function calculaTarifa(bool $cliente_vip, FranquiasDeBagagem $franquias, float $tarifa_franquia)
+    /** Getters
+     *
+     */
+
+    /** Retorna o código do Voo
+     * @return CodigoVoo
+     */
+    public function getCodigo(): CodigoVoo
+    {
+        return $this->codigo;
+    }
+
+    /** Retorna a CompanhiaAerea do Voo
+     * @return SiglaCompanhiaAerea
+     */
+    public function getCompanhiaAerea(): SiglaCompanhiaAerea
+    {
+        return $this->companhia_aerea;
+    }
+
+    /** Retorna o Aeroporto de saida do Voo
+     * @return SiglaAeroporto
+     */
+    public function getAeroportoSaida(): SiglaAeroporto
+    {
+        return $this->aeroporto_de_saida;
+    }
+
+    /** Retorna o Aeroporto de chegada do Voo
+     * @return SiglaAeroporto
+     */
+    public function getAeroportoChegada(): SiglaAeroporto
+    {
+        return $this->aeroporto_de_chegada;
+    }
+
+    /** Retorna a hora de partida do Voo
+     * @return Tempo
+     */
+    public function getHoraDePartida(): Tempo
+    {
+        return $this->hora_de_partida;
+    }
+
+    /** Retorna a duração estimada do Voo
+     * @return Duracao
+     */
+    public function getDuracaoEstimada(): Duracao
+    {
+        return $this->duracao_estimada;
+    }
+
+    /** Retorna os dias da semana do Voo
+     * @return DiaDaSemana[]
+     */
+    public function getDiasDaSemana(): array
+    {
+        return $this->dias_da_semana;
+    }
+
+    /** Retorna a Aeronave padrão do Voo
+     * @return RegistroDeAeronave
+     */
+    public function getAeronavePadrao(): RegistroDeAeronave
+    {
+        return $this->aeronave_padrao;
+    }
+
+    /** Retorna a capacidade de passageiros do Voo
+     * @return int
+     */
+    public function getCapacidadeDePassageiros(): int
+    {
+        return $this->capacidade_passageiros;
+    }
+
+    /** Retorna a capacidade de carga do Voo
+     * @return float
+     */
+    public function getCapacidadeCarga(): float
+    {
+        return $this->capacidade_carga;
+    }
+
+    /** Retorna a tarifa do Voo
+     * @return float
+     */
+    public function getTarifa(): float
+    {
+        return $this->tarifa;
+    }
+
+    /** Methods
+     *
+     */
+
+    /** Calcula a tarifa para um cliente com franquias
+     * @param bool $cliente_vip
+     * @param FranquiasDeBagagem $franquias
+     * @param float $tarifa_franquia
+     * @return float
+     */
+    public function calculaTarifa(bool $cliente_vip, FranquiasDeBagagem $franquias, float $tarifa_franquia): float
     {
         return calculo_tarifa_strategy_for($cliente_vip, $this->tarifa, $tarifa_franquia)->calcula($franquias);
     }
 
+    /** Constroi os assentos para a capacidade de passageiros desse Voo
+     * @return Assento[]
+     */
     public function construirAssentos(): array
     {
         $gerador = new GeradorDeCodigoDoAssento($this->capacidade_passageiros, 0.0);
         $assentos = $gerador->gerar_todos();
         $dict_assentos = array();
         foreach ($assentos as $codigo_assento) {
-            $dict_assentos[$codigo_assento] = new Assento($codigo_assento);
+            $dict_assentos["{$codigo_assento}"] = new Assento($codigo_assento);
         }
         return $dict_assentos;
-    }
-
-    public function getCodigo(): CodigoVoo
-    {
-        return $this->codigo;
-    }
-
-    public function getCompanhiaAerea(): CompanhiaAerea
-    {
-        return $this->companhia_aerea;
-    }
-
-    public function getAeroportoSaida(): SiglaAeroporto
-    {
-        return $this->aeroporto_de_saida;
-    }
-
-    public function getAeroportoChegada(): SiglaAeroporto
-    {
-        return $this->aeroporto_de_chegada;
-    }
-
-    public function getHoraDePartida(): Data
-    {
-        return $this->hora_de_partida;
-    }
-
-    public function getDuracaoEstimada(): Duracao
-    {
-        return $this->duracao_estimada;
-    }
-
-    public function getDiasDaSemana(): DiaDaSemana
-    {
-        return $this->dias_da_semana;
-    }
-
-    public function getAeronavePadrao(): RegistroDeAeronave
-    {
-        return $this->aeronave_padrao;
-    }
-
-    public function getCapacidadeDePassageiros(): int
-    {
-        return $this->capacidade_passageiros;
-    }
-
-    public function getCapacidadeCarga(): float
-    {
-        return $this->capacidade_carga;
-    }
-
-    public function getTarifa(): float
-    {
-        return $this->tarifa;
     }
 }
 

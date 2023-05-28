395dac55d917a887ae8801ba8e2fcf81fe61829e
Bruno Lima <brunolima280103@gmail.com>
2023-05-27 21:59:16 -0300

    implementando calcula de distancia

--- NOVO/classes/aeroporto.php
+++ NOVO/classes/aeroporto.php
@@ -1,9 +1,7 @@
 <?php
 require_once ("estado.php");
 require_once ("identificadores.php");
-require_once ("coordenada.php");
-
-use Coordenada;
+require_once 'coordenada.php';
 
 class Aeroporto
 {
@@ -11,19 +9,15 @@ class Aeroporto
     private string $cidade;
     private Estado $estado;
 
-    private Coordenada $local_aeroporto;
-
     public function __construct(
         SiglaAeroporto $sigla,
         string $cidade,
         Estado $estado,
-        Coordenada $local_aeroporto
     )
     {
         $this->sigla = $sigla;
         $this->cidade = $cidade;
         $this->estado = $estado;
-        $this->local_aeroporto = $local_aeroporto;
     }
 
     public function getSigla(): SiglaAeroporto
@@ -41,8 +35,6 @@ class Aeroporto
         return $this->estado;
     }
 
-    public function getCoordenada(): Coordenada
-    {
-        return $this->local_aeroporto;
-    }
+
+
 }

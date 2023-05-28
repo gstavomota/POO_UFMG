dfd7f65ed63b8f942591eca798d17d5009b69c9f
Raissa <95766853+raissagd@users.noreply.github.com>
2023-05-28 13:45:03 -0300

    teste: classe passageiro

--- NOVO/test/passageiro.php
+++ NOVO/test/passageiro.php
@@ -1,32 +1,56 @@
 <?php
-include_once 'suite.php';
-include_once '../classes/passageiro.php';
+    include_once 'suite.php';
+    include_once '../classes/passageiro.php';
+    require_once '../classes/passagem.php';
+    class PassageiroTestCase extends TestCase {
 
-class PassageiroTestCase extends TestCase {
+        protected function getName(): string {
+                return "Passageiro";
+        }
 
-    protected function getName(): string {
-            return "Passageiro";
+        public function run() {
+            # Constructor
+            $this->startSection("Constructor");
+            $validNome = 'João';
+            $validSobrenome = 'Silva';
+            $validNacionalidade = Nacionalidade::BRASIL;
+            $invalidNacionalidade = Nacionalidade::ARGENTINA;
+            $validCPF = new CPF("239.111.040-57");
+            $validEmail = new Email('joao@gmail.com');
+            $validData = new Data(2023, 05, 01);
+            $passaporte = new Passaporte('A00000000');
+            $documentoCliente = new DocumentoPassageiro($passaporte);
+            $validPassagens = array(0);
+
+            try {
+                // nome vazio
+                new Passageiro('', $validSobrenome, $documentoCliente, $validNacionalidade, $validCPF, $validData, $validEmail, $validPassagens);
+                $this->checkNotReached();
+            } catch (InvalidArgumentException $e) {
+                $this->checkReached();
+            }
+            try {
+                // sobrenome vazio
+                new Passageiro($validNome, '', $documentoCliente, $validNacionalidade, $validCPF, $validData, $validEmail, $validPassagens);
+                $this->checkNotReached();
+            } catch (InvalidArgumentException $e) {
+                $this->checkReached();
+            }
+            try {
+                // há um cpf, mas a pessoa não é brasileira
+                new Passageiro($validNome, $validSobrenome, $documentoCliente, $invalidNacionalidade, $validCPF, $validData, $validEmail, $validPassagens);
+                $this->checkNotReached();
+            } catch (InvalidArgumentException $e) {
+                $this->checkReached();
+            }
+            try {
+                // a pessoa é brasileira, e há um cpf
+                new Passageiro($validNome, $validSobrenome, $documentoCliente, $validNacionalidade, $validCPF, $validData, $validEmail, $validPassagens);
+                $this->checkReached();
+            } catch (InvalidArgumentException $e) {
+                $this->checkNotReached();
+            }
+        }
     }
 
-    public function run() {
-    # Constructor
-    $this->startSection("Constructor");
-    try {
-        // TODO
-        $this->checkNotReached();
-    } catch (InvalidArgumentException $e) {
-        $this->checkReached();
-    }
-    try {
-        // TODO
-        $this->checkReached();
-    } catch (InvalidArgumentException $e) {
-        $this->checkNotReached();
-    }
-
-    # Valida_cpf_se_brasileiro
-    // TODO
-    $this->startSection("Valida_cpf_se_brasileiro");
-    // TODO
-    }
-}
+?>

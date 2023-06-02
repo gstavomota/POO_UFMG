a7e87e55ed2948553dc790b98493f9967ebb4180
KalilDev <kalilgamer0@gmail.com>
2023-06-02 20:16:14 -0300

    test: calculo_tarifa_strategy: implement stubs for the correct way to do it

--- NOVO/test/calculo_tarifa_strategy.php
+++ NOVO/test/calculo_tarifa_strategy.php
@@ -2,49 +2,75 @@
 include_once 'suite.php';
 include_once '../classes/calculo_tarifa_strategy.php';
 
-class PassageiroComumCalculoTarifaTestCase extends TestCase
-{
+class PassageiroComumCalculoTarifaStrategyTestCase extends TestCase {
+
     protected function getName(): string
     {
-        return "CalculaTarifaStrategy";
+        return "PassageiroComumCalculoTarifaStrategy";
     }
 
     public function run()
     {
-        $this->startSection("Calcula Tarifa De Passageiro Comum");
-        try {
-
-            $passageiro = new PassageiroComumCalculoTarifaStrategy(10.0, 15.0);
-
-            $franquia1 = new FranquiaDeBagagem(19.0);
-            $franquia2 = new FranquiaDeBagagem(21.0);
-            $arr = [$franquia1, $franquia2];
-            $franquias = new FranquiasDeBagagem($arr);
-
-            
-            $this->checkEq(300.0, $passageiro->calcula($franquias));
-            $this->checkReached();
-
-        } catch (InvalidArgumentException $e) {
-            $this->checkReached();
-        }
-
-        $this->startSection("Calcula Tarifa De Passageiro Vip");
-        try {
-
-            $passageiro = new PassageiroVipCalculoTarifaStrategy(10.0, 15.0);
-
-            $franquia1 = new FranquiaDeBagagem(19.0);
-            $franquia2 = new FranquiaDeBagagem(21.0);
-            $arr = [$franquia1, $franquia2];
-            $franquias = new FranquiasDeBagagem($arr);
-
-            
-            $this->checkEq(300.0, $passageiro->calcula($franquias));
-            $this->checkReached();
-
-        } catch (InvalidArgumentException $e) {
-            $this->checkReached();
-        }
+        // TODO: Implement run() method.
     }
 }
+
+class PassageiroVipCalculoTarifaStrategyTestCase extends TestCase {
+
+    protected function getName(): string
+    {
+        return "PassageiroVipCalculoTarifaStrategy";
+    }
+
+    public function run()
+    {
+        // TODO: Implement run() method.
+    }
+}
+
+//class PassageiroComumCalculoTarifaTestCase extends TestCase
+//{
+//    protected function getName(): string
+//    {
+//        return "CalculaTarifaStrategy";
+//    }
+//
+//    public function run()
+//    {
+//        $this->startSection("Calcula Tarifa De Passageiro Comum");
+//        try {
+//
+//            $passageiro = new PassageiroComumCalculoTarifaStrategy(10.0, 15.0);
+//
+//            $franquia1 = new FranquiaDeBagagem(19.0);
+//            $franquia2 = new FranquiaDeBagagem(21.0);
+//            $arr = [$franquia1, $franquia2];
+//            $franquias = new FranquiasDeBagagem($arr);
+//
+//
+//            $this->checkEq(300.0, $passageiro->calcula($franquias));
+//            $this->checkReached();
+//
+//        } catch (InvalidArgumentException $e) {
+//            $this->checkReached();
+//        }
+//
+//        $this->startSection("Calcula Tarifa De Passageiro Vip");
+//        try {
+//
+//            $passageiro = new PassageiroVipCalculoTarifaStrategy(10.0, 15.0);
+//
+//            $franquia1 = new FranquiaDeBagagem(19.0);
+//            $franquia2 = new FranquiaDeBagagem(21.0);
+//            $arr = [$franquia1, $franquia2];
+//            $franquias = new FranquiasDeBagagem($arr);
+//
+//
+//            $this->checkEq(300.0, $passageiro->calcula($franquias));
+//            $this->checkReached();
+//
+//        } catch (InvalidArgumentException $e) {
+//            $this->checkReached();
+//        }
+//    }
+//}

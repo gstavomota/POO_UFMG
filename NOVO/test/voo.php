183a15fcc146b61d3f3f37cf4ec8c0786c55c79c
KalilDev <kalilgamer0@gmail.com>
2023-06-04 16:00:28 -0300

    test: voo: implement correctly

--- NOVO/test/voo.php
+++ NOVO/test/voo.php
@@ -11,79 +11,43 @@ class VooTestCase extends TestCase
 
     public function run()
     {
-        # Construtor 
-        $this->startSection("Calcula Tarifa");
-        try {
+        # Calcula Tarifa
+        $siglaCA = new SiglaCompanhiaAerea("TM");
+        $siglaAeroSaida = new SiglaAeroporto("CON");
+        $siglaAeroChegada = new SiglaAeroporto("GUA");
+        $horaPartida = new Tempo(12, 40, 25);
+        $codigo = new CodigoVoo($siglaCA, 1);
+        $duracaoVoo = new Duracao(25, 30);
+        $diasDaSemana = [DiaDaSemana::SEGUNDA];
+        $aeroPadrao = new RegistroDeAeronave(PrefixoRegistroDeAeronave::PP, "AAA");
+        $tarifa_voo = 300.0;
+        $voo = new Voo($codigo,
+            $siglaCA,
+            $siglaAeroSaida,
+            $siglaAeroChegada,
+            $horaPartida,
+            $duracaoVoo,
+            $diasDaSemana,
+            $aeroPadrao,
+            2,
+            758.7,
+            $tarifa_voo,
+        10
+        );
 
-            $siglaCA = new SiglaCompanhiaAerea("LATAM");
-            $siglaAeroSaida = new SiglaAeroporto("CON");
-            $siglaAeroChegada = new SiglaAeroporto("GUA");
-            $horaPartida = new Tempo(12, 40, 25);
-            $codigo = new CodigoVoo($siglaCA, 1);
-            $duracaoVoo = new Duracao(25, 30);
-            //$diasDaSemana = DiaDaSemana; Implementar enum php
-            //Falta PrefixoREgistroDeAeronave
-            $aeroPadrao = new RegistroDeAeronave();
+        $franquia1 = new FranquiaDeBagagem(19.0);
+        $franquia2 = new FranquiaDeBagagem(21.0);
+        $franquias = new FranquiasDeBagagem([$franquia1, $franquia2]);
+        $tarifa_franquia = 10.0;
+        $this->startSection("calculaTarifa");
+        $this->checkEq(2*$tarifa_franquia+$tarifa_voo, $voo->calculaTarifa(false, $franquias, $tarifa_franquia ));
+        $this->checkEq($tarifa_franquia/2+$tarifa_voo, $voo->calculaTarifa(true, $franquias, $tarifa_franquia ));
 
-            $voo = new Voo($codigo, 
-                $siglaCA, 
-                $siglaAeroSaida, 
-                $siglaAeroChegada, 
-                $horaPartida, 
-                $duracaoVoo, 
-                [1,2,3], 
-                $aeroPadrao,
-                35,
-                758.7,
-                300.0
-            );
-
-            $franquia1 = new FranquiaDeBagagem(19.0);
-            $franquia2 = new FranquiaDeBagagem(21.0);
-            $arr = [$franquia1, $franquia2];
-            $franquias = new FranquiasDeBagagem($arr);
-
-            $this->checkEq(40.0, $voo->calculaTarifa(false, $franquias, 13 ));
-            $this->checkReached();
-
-        } catch (InvalidArgumentException $e) {
-            $this->checkReached();
-        }
-
+        # Construir assentos
         $this->startSection("construirAssentos");
-        try {
-
-            $siglaCA = new SiglaCompanhiaAerea("LATAM");
-            $siglaAeroSaida = new SiglaAeroporto("CON");
-            $siglaAeroChegada = new SiglaAeroporto("GUA");
-            $horaPartida = new Tempo(12, 40, 25);
-            $codigo = new CodigoVoo($siglaCA, 1);
-            $duracaoVoo = new Duracao(25, 30);
-            //$diasDaSemana = DiaDaSemana; Implementar enum php
-            //Falta PrefixoREgistroDeAeronave
-            $aeroPadrao = new RegistroDeAeronave();
-
-            $voo = new Voo($codigo, 
-                $siglaCA, 
-                $siglaAeroSaida, 
-                $siglaAeroChegada, 
-                $horaPartida, 
-                $duracaoVoo, 
-                [1,2,3], 
-                $aeroPadrao,
-                1,
-                758.7,
-                300.0
-            );
-
-            $gerador = new GeradorDeCodigoDoAssento($voo->capacidade_passageiros, 0.0);
-            $assentos = $gerador->gerar_todos();
-
-            $this->checkEq(["{A1}",$assentos[0]], $voo->construirAssentos());
-            $this->checkReached();
-
-        } catch (InvalidArgumentException $e) {
-            $this->checkNotReached();
-        }
+        $gerador = new GeradorDeCodigoDoAssento($voo->getCapacidadeDePassageiros());
+        $assentos = $gerador->gerar_todos();
+        $voo_assentos = $voo->construirAssentos();
+        $this->checkEq($assentos, $voo_assentos->keys());
     }
 }

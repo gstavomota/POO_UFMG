26dd82546fe56b804ada9599e82973ba5bfdfe96
KalilDev <kalilgamer0@gmail.com>
2023-06-05 09:04:33 -0300

    aeroporto: extend persist

--- NOVO/classes/aeroporto.php
+++ NOVO/classes/aeroporto.php
@@ -2,27 +2,38 @@
 require_once ("estado.php");
 require_once ("identificadores.php");
 require_once 'coordenada.php';
+require_once "persist.php";
 
-class Aeroporto
+class Aeroporto extends persist
 {
     private SiglaAeroporto $sigla;
+    private string $nome;
     private string $cidade;
     private Estado $estado;
+    private static string $local_filename = "aeroporto.txt";
 
     public function __construct(
         SiglaAeroporto $sigla,
+        string $nome,
         string $cidade,
         Estado $estado,
+        ...$args
     )
     {
         $this->sigla = $sigla;
+        $this->nome = $nome;
         $this->cidade = $cidade;
         $this->estado = $estado;
+        parent::__construct(...$args);
     }
 
     public function getSigla(): SiglaAeroporto
     {
         return $this->sigla;
+    }
+
+    public function getNome(): string {
+        return $this->nome;
     }
 
     public function getCidade(): string
@@ -36,5 +47,8 @@ class Aeroporto
     }
 
 
-
+    static public function getFilename(): string
+    {
+        return self::$local_filename;
+    }
 }
